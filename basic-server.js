44109a3de53ef0fc01295cd7bff10e75774c494e
m4rreParre <marrebjoredal@gmail.com>
2025-02-19 22:29:44 +0100

    Refactor basic server code to remove unnecessary comments and improve readability

--- basic-server.js
+++ basic-server.js
@@ -4,13 +4,10 @@ const fs = require('fs');
 const path = require('path');
 
 const server = http.createServer(function(req, res) {
-    // Set the base directory for static files
     const baseDir = path.join(__dirname, 'public');
 
-    // Construct the file path based on the request URL
     let filePath = path.join(baseDir, req.url === '/' ? 'index.html' : req.url);
 
-    // Get the file extension
     const extname = String(path.extname(filePath)).toLowerCase();
     const mimeTypes = {
         '.html': 'text/html',
@@ -29,10 +26,8 @@ const server = http.createServer(function(req, res) {
         '.svg': 'application/image/svg+xml'
     };
 
-    // Set the content type based on the file extension
     const contentType = mimeTypes[extname] || 'application/octet-stream';
 
-    // Read and serve the file
     fs.readFile(filePath, (err, content) => {
         if (err) {
             if (err.code === 'ENOENT') {
@@ -42,7 +37,6 @@ const server = http.createServer(function(req, res) {
                     res.end(content, 'utf8');
                 });
             } else {
-                // Some server error
                 res.writeHead(500);
                 res.end(`Server Error: ${err.code}`);
             }
